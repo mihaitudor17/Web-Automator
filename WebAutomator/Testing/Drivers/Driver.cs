2b7df2ac54088f5c8619f8f0668190b5e4308568
mihaitudor17 <blagamihai17@gmail.com>
2023-05-23 10:37:53 +0300

    Added the initial project structure.

---
+++ WebAutomator/Testing/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Testing.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ WebAutomator/Testing/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace Testing.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
